bcd9a5fd2b3c7bc817720033333a5f9dc6484a4a
Aaron Li <aaronli@holygpu8a18602.rc.fas.harvard.edu>
2025-05-06 23:18:06 -0400

    refactoring 1/4 done

---
+++ utils.py
@@ -0,0 +1,72 @@
+import sys
+sys.path.append('../sae')
+from sae import Sae
+from sae_lens import SAE
+from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaForCausalLM
+import torch
+import torch.nn.functional as F
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import time
+import argparse
+from torch import nn, Tensor
+from huggingface_hub import hf_hub_download, login
+import transformers
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+BASE_DIR = "/n/netscratch/hlakkaraju_lab/Lab/aaronli/sae/"
+CACHE_DIR = "/n/netscratch/hlakkaraju_lab/Lab/aaronli/models/"
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+def count_common(x, y):
+    return sum(1 for elem in x if elem in y)
+
+def jump_relu(x, theta):
+    return x * (x > theta).float()
+
+def load_model_and_sae(model_type, layer_num, device=DEVICE):
+    if model_type == "llama3-8b":
+        tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B", cache_dir=CACHE_DIR)
+        model = LlamaForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B", cache_dir=CACHE_DIR).to(device)
+        sae = Sae.load_from_disk(BASE_DIR + f"layers.{layer_num}").to(DEVICE)
+    elif model_type == "gemma2-9b":
+        tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-9b", cache_dir=CACHE_DIR)
+        model = AutoModelForCausalLM.from_pretrained("google/gemma-2-9b", cache_dir=CACHE_DIR).to(DEVICE)
+        assert layer_num == 30
+        path_to_params = hf_hub_download(
+            repo_id="google/gemma-scope-9b-pt-res",
+            filename="layer_30/width_131k/average_l0_170/params.npz",
+            force_download=False,
+            cache_dir = "/n/netscratch/hlakkaraju_lab/Lab/aaronli/gemma2-9b-sae"
+        )
+        params = np.load(path_to_params)
+        sae = {k: torch.from_numpy(v).cuda() for k, v in params.items()}
+        # W_enc = pt_params['W_enc']
+        # b_enc = pt_params['b_enc']
+        # theta = pt_params['threshold']
+    else:
+        raise ValueError(f"Unsupported model_type: {model_type}")
+    model.config.pad_token_id = model.config.eos_token_id
+    model.eval()
+    return model, tokenizer, sae
+
+def extract_sae_features(h, sae, model_type, k):
+    is_batch = (len(h.shape) == 3)
+    if model_type == "llama3-8b":
+        z = sae.pre_acts(h)
+        s = sae.encode(h).top_indices
+        s_acts = sae.encode(h).top_acts
+    elif model_type == "gemma2-9b":
+        z = jump_relu(h @ sae['W_enc'] + sae['b_enc'], sae['theta'])
+        sorted_acts, indices = torch.sort(z, dim=-1, descending=True)
+        if is_batch:
+            s = indices[:, :k]
+            s_acts = sorted_acts[:, :k]
+        else:
+            s = indices[:k]
+            s_acts = sorted_acts[:k]
+    else:
+        raise ValueError(f"Unsupported model type: {model_type}")
+    return z, s, s_acts
